e51c904d3f99af541116f356a47d3fdac4090103
Daulet <dauletkd01@gmail.com>
2023-09-20 17:12:22 +0600

    configured zipkin, updated spring boot version to 3rd

--- ProductService/src/main/java/com/example/productservice/controller/ProductController.java
+++ ProductService/src/main/java/com/example/productservice/controller/ProductController.java
@@ -8,9 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
-
 @RestController
 @RequestMapping("/product")
 public class ProductController {
