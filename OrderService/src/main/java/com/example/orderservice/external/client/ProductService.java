a539046a8e18739a9d621acffec8536e1c8eb7a3
Daulet <dauletkd01@gmail.com>
2023-09-21 13:55:28 +0600

    Implemented CircuitBreaker in OrderService

--- OrderService/src/main/java/com/example/orderservice/external/client/ProductService.java
+++ OrderService/src/main/java/com/example/orderservice/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.orderservice.external.client;
 
+import com.example.orderservice.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
@@ -13,4 +16,12 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default void fallback(Exception e) {
+        throw new CustomException(
+                "Product Service is not available",
+                "UNAVAILABLE",
+                500
+        );
+    }
 }
