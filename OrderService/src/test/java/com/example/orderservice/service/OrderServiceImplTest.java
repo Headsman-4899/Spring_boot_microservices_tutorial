bc1dfc1baf29d0a90a9cd4208734fb653d68bb48
Daulet <dauletkd01@gmail.com>
2023-09-22 15:52:38 +0600

    Added test for getOrder (success scenario)

---
+++ OrderService/src/test/java/com/example/orderservice/service/OrderServiceImplTest.java
@@ -0,0 +1,105 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.entity.Order;
+import com.example.orderservice.external.client.PaymentService;
+import com.example.orderservice.external.client.ProductService;
+import com.example.orderservice.external.response.PaymentResponse;
+import com.example.orderservice.external.response.ProductResponse;
+import com.example.orderservice.model.OrderResponse;
+import com.example.orderservice.model.PaymentMode;
+import com.example.orderservice.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class OrderServiceImplTest {
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private ProductService productService;
+    @Mock
+    private PaymentService paymentService;
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    OrderService orderService = new OrderServiceImpl();
+
+    @DisplayName("Get Order - Success Scenario")
+    @Test
+    void test_when_order_success() {
+        // Mocking
+        Order order = getMockOrder();
+        when(orderRepository.findById(anyLong()))
+                .thenReturn(Optional.of(order));
+
+        when(restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/product/" + order.getProductId(),
+                ProductResponse.class
+        )).thenReturn(getMockOrderResponse());
+
+        when(restTemplate.getForObject(
+                "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class
+        )).thenReturn(getMockPaymentResponse());
+
+        // Actual
+        OrderResponse orderResponse = orderService.getOrderDetails(1);
+
+        // Verification
+        verify(orderRepository, times(1)).findById(anyLong());
+        verify(restTemplate, times(1)).getForObject(
+                "http://PRODUCT-SERVICE/product/" + order.getProductId(),
+                ProductResponse.class);
+        verify(restTemplate, times(1)).getForObject(
+                "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class);
+
+        // Assert
+        assertNotNull(orderResponse);
+        assertEquals(order.getId(), orderResponse.getOrderId());
+    }
+
+    private PaymentResponse getMockPaymentResponse() {
+        return PaymentResponse.builder()
+                .paymentId(1)
+                .paymentDate(Instant.now())
+                .paymentMode(PaymentMode.CASH)
+                .amount(200)
+                .orderId(1)
+                .status("ACCEPTED")
+                .build();
+    }
+
+    private ProductResponse getMockOrderResponse() {
+        return ProductResponse.builder()
+                .productId(2)
+                .productName("iPhone 14 Pro")
+                .price(999)
+                .quantity(200)
+                .build();
+    }
+
+    private Order getMockOrder() {
+        return Order.builder()
+                .orderStatus("PLACED")
+                .orderDate(Instant.now())
+                .id(1)
+                .amount(100)
+                .quantity(200)
+                .productId(2)
+                .build();
+    }
+}
