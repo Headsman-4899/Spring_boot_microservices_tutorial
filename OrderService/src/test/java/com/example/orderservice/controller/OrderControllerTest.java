6be29715f26f2c52a4acaa5e46b6843e44376ca0
Daulet <dauletkd01@gmail.com>
2023-09-24 16:32:11 +0600

    Created OrderControllerTest class

---
+++ OrderService/src/test/java/com/example/orderservice/controller/OrderControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.OrderServiceConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest({"server.port=0"})
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {OrderServiceConfig.class})
+class OrderControllerTest {
+
+}
