98c5e4ebb83995d528a18a383f4b6c517a6c1c35
Daulet <dauletkd01@gmail.com>
2023-09-24 16:38:39 +0600

    Added dependencies in OrderControllerTest

--- OrderService/src/test/java/com/example/orderservice/controller/OrderControllerTest.java
+++ OrderService/src/test/java/com/example/orderservice/controller/OrderControllerTest.java
@@ -1,15 +1,42 @@
 package com.example.orderservice.controller;
 
 import com.example.orderservice.OrderServiceConfig;
+import com.example.orderservice.repository.OrderRepository;
+import com.example.orderservice.service.OrderService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = {OrderServiceConfig.class})
 class OrderControllerTest {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(WireMockConfiguration
+                    .wireMockConfig()
+                    .port(8080))
+            .build();
+
+    private ObjectMapper objectMapper = new ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 }
